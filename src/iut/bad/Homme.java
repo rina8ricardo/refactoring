f2ed5bdf5f67a5c8ed416fde50b76193eb6986e9
rina8ricardo <rianarak8@gmail.com>
2024-03-30 01:36:37 -0700

    creation du package iut.bad et des classes Homme Femme

---
+++ src/iut/bad/Homme.java
@@ -0,0 +1,16 @@
+package iut.bad;
+
+public class Homme {
+	private String nom; 
+	private String prenom; 
+	private int age;
+	
+	public Homme(String nom, String prenom, int age) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+		this.age = age;
+	} 
+
+	
+}
